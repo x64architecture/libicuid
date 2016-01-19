f00c8867c1c59ba12630661cde3c1cbff12f0295
Kurt Cancemi <kurt@x64architecture.com>
2016-01-18 19:37:56 -0500

    Support Visual Studio 2015

--- cmake/ConfigureChecks.cmake
+++ cmake/ConfigureChecks.cmake
@@ -1,7 +1,8 @@
 include (CheckFunctionExists)
+include (CheckSymbolExists)
 
-CHECK_FUNCTION_EXISTS(strlcpy      HAVE_STRLCPY)
-CHECK_FUNCTION_EXISTS(snprintf     HAVE_SNPRINTF)
+CHECK_FUNCTION_EXISTS(strlcpy           HAVE_STRLCPY)
+CHECK_SYMBOL_EXISTS(snprintf "stdio.h"  HAVE_SNPRINTF)
 
 if (HAVE_STRLCPY)
     add_definitions(-DHAVE_STRLCPY)
