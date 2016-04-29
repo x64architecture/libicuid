879aa82502532aa197c016ea6a3242131d67c3a3
Kurt Cancemi <kurt@x64architecture.com>
2016-04-29 08:52:31 -0400

    Stable 1.4.1

--- include/icuid/icuid_ver.h
+++ include/icuid/icuid_ver.h
@@ -19,11 +19,11 @@
 
 #define LIBICUID_COPYRIGHT "2015 - 2016, Kurt Cancemi (kurt@x64architecture.com)"
 
-#define LIBICUID_VERSION "1.4.0"
+#define LIBICUID_VERSION "1.4.1"
 
 #define LIBICUID_VERSION_MAJOR 1
 #define LIBICUID_VERSION_MINOR 4
-#define LIBICUID_VERSION_PATCH 0
+#define LIBICUID_VERSION_PATCH 1
 
 #endif /* __LIBICUID_VER_H__ */
 
