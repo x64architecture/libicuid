0c5aa97b4193d4238f2e9d9344236e4fca75a770
Kurt Cancemi <kurt@x64architecture.com>
2016-08-17 16:05:18 -0400

    Use stdint.h on Visual Studio 2012 or higher

--- include/icuid/icuid_types.h
+++ include/icuid/icuid_types.h
@@ -21,28 +21,17 @@
 extern "C" {
 #endif
 
-#ifndef _MSC_VER
-#include <stdint.h>
+#if !defined(_MSC_VER) || _MSC_VER >= 1700
+ #include <stdint.h>
 #else
-
-#if (_MSC_VER < 1300)
-   typedef signed char       int8_t;
-   typedef signed short      int16_t;
-   typedef signed int        int32_t;
-   typedef unsigned char     uint8_t;
-   typedef unsigned short    uint16_t;
-   typedef unsigned int      uint32_t;
-#else
-   typedef signed __int8     int8_t;
-   typedef signed __int16    int16_t;
-   typedef signed __int32    int32_t;
-   typedef unsigned __int8   uint8_t;
-   typedef unsigned __int16  uint16_t;
-   typedef unsigned __int32  uint32_t;
-#endif
-typedef signed __int64       int64_t;
-typedef unsigned __int64     uint64_t;
-
+ typedef signed __int8     int8_t;
+ typedef signed __int16    int16_t;
+ typedef signed __int32    int32_t;
+ typedef unsigned __int8   uint8_t;
+ typedef unsigned __int16  uint16_t;
+ typedef unsigned __int32  uint32_t;
+ typedef signed __int64    int64_t;
+ typedef unsigned __int64  uint64_t;
 #endif /* _MSC_VER */
 
 #ifdef __cplusplus
